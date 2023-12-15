bc8a455b58a11e724854f998cf725e4872c1a496
Ben <ben1002.ma@gmail.com>
2023-12-15 18:41:22 -0500

    added pong ai file and directionComponent for 360 degree ball movement

--- pong_ai.py
+++ pong_ai.py
@@ -18,8 +18,6 @@ class PongGame:
         self.player = pygame.Rect(self.screen_w - 20, self.screen_h/2 - 70, 10, 140)
         self.opponent = pygame.Rect(10, self.screen_h/2 - 70, 10, 140)
 
-        angle = random.randrange(360)
-
         self.ball_velocity_y = 7 * random.choice((1, -1))
         self.ball_velocity_x = 7 * random.choice((1, -1))
 
@@ -33,29 +31,42 @@ class PongGame:
 
         self.frame_iteration = 0
 
-        
-
-    def player_move(self):
+    def play_move(self, action):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP: 
-                    self.player_speed -= 7
-                if event.key == pygame.K_DOWN:
-                    self.player_speed += 7
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP:
-                    self.player_speed += 7
-                if event.key == pygame.K_DOWN:
-                    self.player_speed -= 7
+
+        self._move(action)
+
+        reward = 0
+
+        #check game over
+        game_over = False
+        if (self.opponent_score == 11 or 
+            self.frame_iteration > 500 * (self.opponent_score + self.player_score)):
+                game_over = True
+                reward = -10
+                return reward, game_over, self.player_score
+        
+        if (self.ball.colliderect(self.player)):
+            reward = 2
+
+        if (self.player_score == 11):
+            reward = 15
+
+        # update ui and clock
+        self._update()
+        self.clock.tick(60)
+
+        return reward, game_over, self.score
+
 
     def ball_animation(self):
         self.ball.x += self.ball_velocity_x
         self.ball.y += self.ball_velocity_y
 
-        if self.ball.top <= 0 or self.ball.bottom >= self.screen_h:
+        if self.ball.top <= 0 or self.ball.bottom >= SCREEN_HEIGHT:
             pygame.mixer.Sound.play(PONG_SOUND)
             self.ball_velocity_y *= -1
 
@@ -66,7 +77,7 @@ class PongGame:
             self.score_time = pygame.time.get_ticks()
 
         # Opponent Score
-        if self.ball.right >= self.screen_w:
+        if self.ball.right >= SCREEN_WIDTH:
             pygame.mixer.Sound.play(SCORE_SOUND)
             self.opponent_score += 1
             self.score_time = pygame.time.get_ticks()
@@ -75,7 +86,7 @@ class PongGame:
             pygame.mixer.Sound.play(PONG_SOUND)
             self.ball_velocity_x *= -1
 
-    def player_animation(self):
+    def _move(self, action):
         self.player.y += self.player_speed
 
         if self.player.top <= 0:
@@ -83,7 +94,7 @@ class PongGame:
         if self.player.bottom >= self.screen_h:
             self.player.bottom = self.screen_h
             
-    def opponent_move(self):
+    def opponent_animation(self):
         if self.opponent.top < self.ball.y:
             self.opponent.y += self.opponent_speed
         if self.opponent.bottom > self.ball.y:
@@ -115,16 +126,16 @@ class PongGame:
             self.ball_velocity_x = 7 * random.choice((1, -1))
             self.score_time = None
 
-    def update(self):
+    def _update(self):
         self.ball_animation()
         self.player_animation()
-        self.opponent_move()
+        self.opponent_animation()
 
         self.screen.fill(GREY)
         pygame.draw.rect(self.screen, LIGHT_GREY, self.player)
         pygame.draw.rect(self.screen, LIGHT_GREY, self.opponent)
         pygame.draw.ellipse(self.screen, LIGHT_GREY, self.ball)
-        pygame.draw.aaline(self.screen, LIGHT_GREY, (self.screen_w/2, 0), (self.screen_w/2, self.screen_h))
+        pygame.draw.aaline(self.screen, LIGHT_GREY, (SCREEN_WIDTH/2, 0), (SCREEN_WIDTH/2, SCREEN_HEIGHT))
 
         if self.score_time:
             self.ball_reset()
@@ -136,4 +147,8 @@ class PongGame:
         self.screen.blit(opponent_text,(600,470))
 
         pygame.display.flip()
-        self.clock.tick(60)
+
+    def gameOver(self):
+        return
+
+            
---
+++ utils.py
@@ -0,0 +1,4 @@
+import math
+
+def directionComponents(angle):
+    return (math.cos(math.degrees(angle)), math.sin(math.degrees(angle)))
